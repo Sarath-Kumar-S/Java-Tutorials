2ab9c7d2e7046c5a5c882a18f3fd0b23b26637c8
Sarath Kumar S <54178445+Sarath-Kumar-S@users.noreply.github.com>
2020-03-03 10:13:21 +0530

    Add files via upload

--- A.java
+++ A.java
@@ -1,18 +1,14 @@
+import java.util.*;
+import java.io.*;
 class A
 {
-int x;
-A()
-{
-x=10;
-System.out.println(x);
-}
-A(int m,int n)
-{
-System.out.println(m+" "+n);
-}
-public static void main(String args[])
-{
-A s1=new A();
-A s2=new A(2,3);
-}
+ public static void main(String args[])
+ {
+  int n;
+  System.out.println("Enter the number:");
+  Scanner sc = new Scanner(System.in);
+  BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+  n=sc.nextInt();
+  System.out.println("You entered " +n);
+ }
 }
